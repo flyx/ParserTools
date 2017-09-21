998faa32abf17e4bad4676846faf3913bbca8319
Felix Krause <contact@flyx.org>
2017-09-21 14:36:51 +0200

    consolidated Text and Lexer into Parser_Tools

---
+++ src/implementation/lexer-source-text_io.adb
@@ -0,0 +1,25 @@
+--  part of AdaYaml, (c) 2017 Felix Krause
+--  released under the terms of the MIT license, see the file "copying.txt"
+
+package body Lexer.Source.Text_IO is
+   procedure Read_Data (S : in out Instance; Buffer : out String;
+                        Length : out Natural) is
+   begin
+      Length := Buffer'First;
+      loop
+         Ada.Text_IO.Get_Line (S.File_Pointer.all,
+                               Buffer (Length .. Buffer'Last), Length);
+         exit when Length = Buffer'Last;
+         if Ada.Text_IO.End_Of_File (S.File_Pointer.all) then
+            Length := Length + 1;
+            Buffer (Length) := Character'Val (4);
+            exit;
+         end if;
+         Buffer (Length + 1) := Character'Val (10);
+         Length := Length + 2;
+      end loop;
+   end Read_Data;
+
+   function As_Source (File : Ada.Text_IO.File_Access) return Pointer is
+     (Pointer'(new Instance'(Source.Instance with File_Pointer => File)));
+end Lexer.Source.Text_IO;
---
+++ src/interface/lexer-source-text_io.ads
@@ -0,0 +1,23 @@
+--  part of AdaYaml, (c) 2017 Felix Krause
+--  released under the terms of the MIT license, see the file "copying.txt"
+
+with Ada.Text_IO;
+
+package Lexer.Source.Text_IO is
+   --  this package provides a source type that is backed by Ada.Text_IO's file
+   --  type. the primary use-case is to read from stdin. use Yaml.Sources.Files
+   --  for standard files, it is more efficient.
+
+   type Instance is new Source.Instance with private;
+
+   overriding procedure Read_Data (S : in out Instance; Buffer : out String;
+                                   Length : out Natural);
+
+   function As_Source (File : Ada.Text_IO.File_Access) return Pointer
+     with Pre => Ada.Text_IO.Is_Open (File.all);
+
+private
+   type Instance is new Source.Instance with record
+      File_Pointer : Ada.Text_IO.File_Access;
+   end record;
+end Lexer.Source.Text_IO;
